a816e4b47ed8fb8ea5d6d03dc92a517566df5275
Merril Mathew <merril.mathew@outlook.com>
2022-09-14 16:28:05 +0100

    Add frontdoor resources
    Front door resource definitions needed to be added as these were not
    available in previously in terraform. This commit adds the capability to
    deploy origin group.

--- domains/environment_domains/variables.tf
+++ domains/environment_domains/variables.tf
@@ -2,4 +2,9 @@ variable "zone" {}
 variable "front_door_name" {}
 variable "resource_group_name" {}
 variable "domains" {}
-variable "tags" {}
+variable "environment" {}
+variable "host_name" {}
+
+locals {
+    endpoint_zone_name = replace(var.zone, ".", "-")
+}
