b1a1f1836ba62df88f2e8e6f34034e7b04848606
RMcVelia <rick.mcvelia@gmail.com>
2023-02-20 16:43:41 +0000

    Add option to override origin_host_header to null

--- domains/environment_domains/variables.tf
+++ domains/environment_domains/variables.tf
@@ -4,6 +4,11 @@ variable "resource_group_name" {}
 variable "domains" {}
 variable "environment" {}
 variable "host_name" {}
+variable "null_host_header" {
+  default     = false
+  description = "The origin_host_header for the azurerm_cdn_frontdoor_origin resource will be var.host_name (if false) or null (if true). If null then the host name from the incoming request will be used."
+}
+
 variable "rule_set_ids" {
   type    = list(any)
   default = null
@@ -15,10 +20,10 @@ variable "multiple_hosted_zones" {
 }
 
 locals {
-  # If true, removes .gov.uk and replaces remaining period with a hypthen e.g. 'domain.education.gov.uk' becomes domain-edu.
+  # If true, removes .gov.uk and replaces remaining period with a hyphen e.g. 'domain.education.gov.uk' becomes domain-edu.
   # We shorten the zone name as the fd endpoint name can only be a maximum of 46 chars
   # This works around an issue where two front doors in the same resource group can't have an endpoint with the same name.
   # If false, removes anything after the first full stop/period e.g. 'domain.education.gov.uk' becomes just 'domain'.
-  short_zone_name = substr(replace(var.zone, "/^[^.]*[.]/", ""), 0, 3)
-  endpoint_zone_name = var.multiple_hosted_zones ? replace(replace(var.zone, "/\\..+$/", "-${local.short_zone_name}"), ".", "-") : replace(var.zone, "/\\..+$/", "")
+  short_zone_name    = substr(replace(var.zone, "/^[^.]+\\./", ""), 0, 3)
+  endpoint_zone_name = var.multiple_hosted_zones ? replace(var.zone, "/\\..+$/", "-${local.short_zone_name}") : replace(var.zone, "/\\..+$/", "")
 }
