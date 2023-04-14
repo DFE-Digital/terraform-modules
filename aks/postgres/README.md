4779ec7607cade407a2d8d46a87871685444bd01
Thomas Leese <thomas@leese.io>
2023-04-14 16:02:50 +0200

    Add AKS Postgres module
    This adds a new module which deploys a PostgreSQL instance, either on
    Azure or via Kubernetes.

---
+++ aks/postgres/README.md
@@ -0,0 +1,51 @@
+# AKS Postgres
+
+Terraform code for deploying a PostgreSQL instance.
+
+## Usage
+
+```terraform
+module "postgres" {
+  source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/postgres?ref=stable"
+
+  name = "database"
+
+  namespace             = var.namespace
+  environment           = "${var.app_environment}${var.app_suffix}"
+  azure_resource_prefix = var.azure_resource_prefix
+  service_short         = var.service_short
+  config_short          = var.config_short
+
+  cluster_configuration_map = module.aks_cluster_data.configuration_map
+
+  use_azure = var.deploy_azure_backing_services
+
+  azure_extensions = ["UNACCENT"]
+}
+```
+
+## Outputs
+
+### `username`
+
+The admin username of the PostgreSQL instance.
+
+### `password`
+
+The admin password of the PostgreSQL instance.
+
+### `host`
+
+The hostname of the PostgreSQL instance.
+
+### `port`
+
+The port of the PostgreSQL instance.
+
+### `name`
+
+The name of the database.
+
+### `url`
+
+The URL used to connect to the PostgreSQL instance.
---
+++ aks/postgres/variables.tf
@@ -0,0 +1,113 @@
+variable "name" {
+  type        = string
+  description = "Name of the instance"
+  default     = null
+}
+
+variable "namespace" {
+  type        = string
+  description = "Current namespace"
+}
+
+variable "environment" {
+  type        = string
+  description = "Current application environment"
+}
+
+variable "azure_resource_prefix" {
+  type        = string
+  description = "Prefix of Azure resources for the service"
+}
+
+variable "service_short" {
+  type        = string
+  description = "Short name of the service"
+}
+
+variable "config_short" {
+  type        = string
+  description = "Short name of the configuration"
+}
+
+variable "cluster_configuration_map" {
+  type = object({
+    resource_group_name = string,
+    resource_prefix     = string,
+    dns_zone_prefix     = optional(string),
+    cpu_min             = number
+  })
+  description = "Configuration map for the cluster"
+}
+
+variable "server_version" {
+  type        = string
+  default     = "14"
+  description = "Version of PostgreSQL server"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Username of the admin user"
+  default     = null
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password of the admin user"
+  sensitive   = true
+  default     = null
+}
+
+variable "use_azure" {
+  type        = bool
+  description = "Whether to deploy using Azure Redis Cache service"
+}
+
+variable "azure_storage_mb" {
+  type    = number
+  default = 32768
+}
+
+variable "azure_sku_name" {
+  type    = string
+  default = "B_Standard_B1ms"
+}
+
+variable "azure_enable_high_availability" {
+  type    = bool
+  default = false
+}
+
+variable "azure_extensions" {
+  type    = list(string)
+  default = []
+}
+
+variable "azure_memory_threshold" {
+  type    = number
+  default = 75
+}
+
+variable "azure_cpu_threshold" {
+  type    = number
+  default = 60
+}
+
+variable "azure_storage_threshold" {
+  type    = number
+  default = 75
+}
+
+variable "azure_enable_monitoring" {
+  type    = bool
+  default = true
+}
+
+variable "azure_maintenance_window" {
+  type = object({
+    day_of_week  = optional(number)
+    start_hour   = optional(number)
+    start_minute = optional(number)
+  })
+  default = null
+}
