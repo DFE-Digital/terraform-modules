a486e132b8cb934c8c29e7494ca676225ecb8d10
Divesh Mavji <divesh987@hotmail.co.uk>
2023-05-10 15:38:24 +0100

    Change url in output to point to redis kubernetes service create explicit dependancy on kubernetes deployment for kubernetes service

--- aks/redis/output.tf
+++ aks/redis/output.tf
@@ -1,4 +1,4 @@
 output "url" {
-  value     = var.use_azure ? "rediss://:${azurerm_redis_cache.main[0].primary_access_key}@${azurerm_redis_cache.main[0].hostname}:${azurerm_redis_cache.main[0].ssl_port}/0" : "redis://${local.kubernetes_name}:6379/0"
+  value     = var.use_azure ? "rediss://:${azurerm_redis_cache.main[0].primary_access_key}@${azurerm_redis_cache.main[0].hostname}:${azurerm_redis_cache.main[0].ssl_port}/0" : "redis://${kubernetes_service.main[0].metadata[0].name}:6379/0"
   sensitive = true
 }
