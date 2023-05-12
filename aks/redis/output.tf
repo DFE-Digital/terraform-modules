e4f1459d3b339890a6711c18e801cdc676de5f65
Divesh Mavji <divesh987@hotmail.co.uk>
2023-05-12 13:37:31 +0100

    Add output dependancy on azure private endpoint

--- aks/redis/output.tf
+++ aks/redis/output.tf
@@ -1,4 +1,4 @@
 output "url" {
-  value     = var.use_azure ? "rediss://:${azurerm_redis_cache.main[0].primary_access_key}@${azurerm_redis_cache.main[0].hostname}:${azurerm_redis_cache.main[0].ssl_port}/0" : "redis://${kubernetes_service.main[0].metadata[0].name}:6379/0"
+  value     = var.use_azure ? "rediss://:${azurerm_redis_cache.main[0].primary_access_key}@${azurerm_private_endpoint.main[0].private_dns_zone_configs[0].record_sets[0].name}.redis.cache.windows.net:${azurerm_redis_cache.main[0].ssl_port}/0" : "redis://${kubernetes_service.main[0].metadata[0].name}:6379/0"
   sensitive = true
 }
