42a88de8ca92a599b36ed04cb3d92791622dd444
Thomas Leese <thomas@leese.io>
2023-06-14 12:38:34 +0100

    Add url and probe_url output
    This adds an output to the AKS application module suitable for
    monitoring the service, useful when passing direct to the StatusCake
    module.

--- aks/application/outputs.tf
+++ aks/application/outputs.tf
@@ -1,3 +1,11 @@
 output "hostname" {
   value = local.hostname
 }
+
+output "url" {
+  value = "https://${local.hostname}"
+}
+
+output "probe_url" {
+  value = var.probe_path != null ? "https://${local.hostname}${var.probe_path}" : null
+}
