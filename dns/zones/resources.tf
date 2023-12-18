6a27418af5432719070764f6153cca3495e3692d
John Ake <johnake@hotmail.com>
2023-12-18 11:43:41 +0000

    Update default CAA records to globalsign and digicert
    Refactor the dns module to handle multiple CAA records

--- dns/zones/resources.tf
+++ dns/zones/resources.tf
@@ -14,9 +14,10 @@ resource "azurerm_dns_zone" "dns_zone" {
 # CAA record
 
 locals {
+  # caa_records is deprecated. Use caa_record_list instead
   caa_records = flatten([
     for zone_name, zone_cfg in var.hosted_zone : [
-      for record_name, record_cfg in zone_cfg["caa_records"] : {
+      for record_name, record_cfg in try(zone_cfg["caa_records"], {}) : {
         record_name         = record_name
         zone_name           = zone_name
         resource_group_name = zone_cfg["resource_group_name"]
@@ -26,8 +27,15 @@ locals {
       }
     ]
   ])
+
+  hosted_zone_with_caa_record_list = {
+    for zone_name, zone_cfg in var.hosted_zone :
+    zone_name => zone_cfg
+    if length(try(zone_cfg.caa_record_list, [])) > 0
+  }
 }
 
+# caa_records is deprecated. Use caa_record_list instead
 resource "azurerm_dns_caa_record" "caa_records" {
   for_each = {
     for zone in local.caa_records : "${zone.zone_name}.${zone.record_name}" => zone
@@ -42,6 +50,29 @@ resource "azurerm_dns_caa_record" "caa_records" {
     flags = each.value.flags
     tag   = each.value.tag
     value = each.value.value
+  }
+
+  depends_on = [
+    azurerm_dns_zone.dns_zone
+  ]
+
+}
+
+resource "azurerm_dns_caa_record" "caa_record_list" {
+  for_each = local.hosted_zone_with_caa_record_list
+
+  name                = "@"
+  zone_name           = each.key
+  resource_group_name = each.value.resource_group_name
+  ttl                 = 300
+
+  dynamic "record" {
+    for_each = toset(each.value.caa_record_list)
+    content {
+      flags = 0
+      tag   = "issue"
+      value = record.value
+    }
   }
 
   depends_on = [
