c57890d4a81bfd88d10f320052d4085c33c22e91
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-03-04 12:12:05 +0000

    Validate SSL on uptime check
    Alert when the certificate is expired

--- monitoring/statuscake/resources.tf
+++ monitoring/statuscake/resources.tf
@@ -13,7 +13,7 @@ resource "statuscake_uptime_check" "main" {
     timeout          = 40
     request_method   = "HTTP"
     status_codes     = ["204", "205", "206", "303", "400", "401", "403", "404", "405", "406", "408", "410", "413", "444", "429", "494", "495", "496", "499", "500", "501", "502", "503", "504", "505", "506", "507", "508", "509", "510", "511", "521", "522", "523", "524", "520", "598", "599"]
-    validate_ssl     = false
+    validate_ssl     = true
   }
 
   monitored_resource {
