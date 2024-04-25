12aafd9d4a1b314d95874863661a22b784620cef
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-04-25 17:33:19 +0100

    endpoints

---
+++ domains/environment_domains/output.tf
@@ -0,0 +1,6 @@
+output "endpoint_host_name" {
+  value = var.add_to_endpoint_host_name == null ? azurerm_cdn_frontdoor_endpoint.main[0].host_name : var.add_to_endpoint_host_name
+}
+output "endpoint_id" {
+  value = var.add_to_endpoint_id == null ? azurerm_cdn_frontdoor_endpoint.main[0].id : var.add_to_endpoint_id
+}
