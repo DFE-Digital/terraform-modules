ae970d064b86342aba54f7336cd2b3153c4b7709
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2025-01-13 14:11:29 +0000

    Output application configuration variables
    Application variables may be useful to read in terraform to configure
    the infrastructure

--- aks/application_configuration/outputs.tf
+++ aks/application_configuration/outputs.tf
@@ -1,7 +1,26 @@
 output "kubernetes_config_map_name" {
-  value = kubernetes_config_map.main.metadata.0.name
+  description = "Name of the kubernetes configmap containing the non-secret environment variables"
+  value       = kubernetes_config_map.main.metadata.0.name
 }
 
 output "kubernetes_secret_name" {
-  value = kubernetes_secret.main.metadata.0.name
+  description = "Name of the kubernetes secret containing the secret environment variables"
+  value       = kubernetes_secret.main.metadata.0.name
+}
+
+output "variables_map" {
+  description = "Map of all non-secret environment variables"
+  value       = local.config_map_data
+}
+
+output "secrets_map" {
+  description = "Map of all secret environment variables"
+  value       = local.secret_data
+  sensitive   = true
+}
+
+output "map" {
+  description = "Map of all environment variables, including secret and non-secret."
+  value       = local.secret_data
+  sensitive   = true
 }
