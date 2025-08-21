166a44dc45f2c05dc8d7270f3214b851e20f37c4
Elaiy Ganesan <elaiyganesan@gmail.com>
2025-08-21 16:43:00 +0100

    Testing the logical replicatiaon monitoring

--- aks/postgres/data.tf
+++ aks/postgres/data.tf
@@ -28,7 +28,7 @@ data "azurerm_resource_group" "monitoring" {
 data "azurerm_monitor_action_group" "main" {
   count = local.azure_enable_monitoring ? 1 : 0
 
-  name                = "${var.azure_resource_prefix}-${var.service_name}"
+  name                = "${var.azure_resource_prefix}-${var.service_short_name}"
   resource_group_name = data.azurerm_resource_group.monitoring[0].name
 }
 
