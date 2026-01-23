17311a7672ccb5fcfdfe37c1b4e5fd5a0f456cd2
paul senior <paul.senior@education.gov.uk>
2026-01-23 16:57:26 +0000

    feat(2615): WIP PR to toggle istio resources on/off

--- aks/redis/variables.tf
+++ aks/redis/variables.tf
@@ -91,7 +91,7 @@ variable "azure_memory_threshold" {
 variable "azure_maxmemory_policy" {
   type     = string
   nullable = false
-  default  = "allkeys-lru"  #"volatile-lru"
+  default  = "allkeys-lru"
 }
 
 variable "azure_enable_monitoring" {
