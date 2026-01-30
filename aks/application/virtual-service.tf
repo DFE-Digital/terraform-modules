9ddc09dc949f56ad6fa8f450073d856f59c69629
paul senior <paul.senior@education.gov.uk>
2026-01-30 16:11:12 +0000

    feat(2615): stio PoC: update application module  to have separate virtual service resource

--- aks/application/virtual-service.tf
+++ aks/application/virtual-service.tf
@@ -7,11 +7,7 @@ locals {
   istio_name_suffix = var.name != null ? "-${var.name}" : ""
   istio_app_name    = "${var.service_name}-${var.environment}${local.istio_name_suffix}"
 
- istio_dns_zone_prefix = (
-    try(var.cluster_configuration_map.dns_zone_prefix, null) != null
-    ? "istio${var.cluster_configuration_map.dns_zone_prefix}"
-    : null
-  )
+ istio_dns_zone_prefix = "istio${var.cluster_configuration_map.dns_zone_prefix}"
 
 }
 
