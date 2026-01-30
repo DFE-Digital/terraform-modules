93684b9dda3a4160d4e1b95dfb1163828111e831
paul senior <paul.senior@education.gov.uk>
2026-01-30 11:59:42 +0000

    feat(2615): stio PoC: update application module  to have separate virtual service resource

--- aks/application/resources.tf
+++ aks/application/resources.tf
@@ -249,33 +249,6 @@ resource "kubernetes_ingress_v1" "main" {
   }
 }
 
-# create istio virtual service for web applications - istio.enabled="true"
-resource "kubernetes_manifest" "istio_virtual_service" {
-  for_each = var.istio_enabled ? toset(local.hostnames) : toset([])
-
-  manifest = {
-    apiVersion = "networking.istio.io/v1beta1"
-    kind       = "VirtualService"
-    metadata = {
-      name      = each.value
-      namespace = var.namespace
-    }
-    spec = {
-      hosts = [each.value]
-      gateways = ["istio-ingress/istio-ingress-gateway"]
-      http = [{
-        route = [{
-          destination = {
-            host = var.send_traffic_to_maintenance_page ? "${var.service_name}-maintenance" : kubernetes_service.main[0].metadata[0].name
-            port = var.send_traffic_to_maintenance_page ? local.maintenance_service_port : kubernetes_service.main[0].spec[0].port[0].port
-          }
-        }]
-      }]
-    }
-  }
-}
-
-
 resource "kubernetes_pod_disruption_budget_v1" "main" {
   count = var.replicas > 1 ? 1 : 0
 
---
+++ aks/application/virtual-service.tf
@@ -0,0 +1,48 @@
+#CREATE istio VirtualService for web applications - istio.enabled="true"
+
+resource "kubernetes_manifest" "istio_virtual_service" {
+  for_each = var.istio_enabled ? toset(local.hostnames) : toset([])
+
+  manifest = {
+    apiVersion = "networking.istio.io/v1beta1"
+    kind       = "VirtualService"
+    metadata = {
+      name      = "vs-${replace(each.value, ".", "-")}"
+      namespace = var.namespace
+    }
+
+    spec = {
+      hosts    = [each.value]
+      gateways = [
+        "istio-ingress/istio-ingress-gateway"
+        ]
+
+      http = [
+        {
+          match = [
+            {
+              uri = { prefix = "/" }
+            }
+          ]
+          route = [
+            {
+              destination = {
+                # NOTE: If the service is in the same namespace, short name is fine.
+                # If not, use FQDN: "${svc}.${ns}.svc.cluster.local"
+                host = var.send_traffic_to_maintenance_page ?
+                  "${var.service_name}-maintenance" :
+                  kubernetes_service.main[0].metadata[0].name
+
+                port = {
+                  number = var.send_traffic_to_maintenance_page ?
+                    local.maintenance_service_port :
+                    kubernetes_service.main[0].spec[0].port[0].port
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
