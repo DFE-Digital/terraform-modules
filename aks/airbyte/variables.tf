2a4523460306a6c9787ebb08e3c54fa23c4e66eb
James Gunn <james@gunn.io>
2026-07-30 14:34:13 +0100

    Add option to skip policy tag application

--- aks/airbyte/variables.tf
+++ aks/airbyte/variables.tf
@@ -157,6 +157,12 @@ variable "additional_policy_tags" {
   description = "A map of policy tag aliases to tag IDs"
 }
 
+variable "skip_policy_tags" {
+  type        = bool
+  default     = false
+  description = "Whether to skip applying policy tags to tables in Big Query"
+}
+
 locals {
   source_name      = "${var.azure_resource_prefix}-${var.service_short}-${var.environment}-pg-source"
   destination_name = "${var.azure_resource_prefix}-${var.service_short}-${var.environment}-bq-destination"
